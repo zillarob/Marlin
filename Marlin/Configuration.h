739a7705eb03e452531a62e975c28fe4453aea13
zillarob <rob052275@hotmail.com>
2021-03-20 23:00:27 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1558,7 +1558,7 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS -3
+#define MANUAL_X_HOME_POS -3
 //#define MANUAL_Y_HOME_POS  1
 //#define MANUAL_Z_HOME_POS 0
 
